d88e192824da4f35b193ce300f2462f2efb9f3a8
Jared <jared.michael.wood@gmail.com>
2018-08-07 04:17:27 -0500

    Register.html is now displaying properly.

--- src/main/java/launchcode/com/ftf/controllers/LandingController.java
+++ src/main/java/launchcode/com/ftf/controllers/LandingController.java
@@ -1,19 +1,29 @@
 package launchcode.com.ftf.controllers;
 
+import launchcode.com.ftf.models.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "landing")
+@RequestMapping(value = "")
 public class LandingController {
 
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("title", "Landing");
+        model.addAttribute("title", "Food Truck Finder");
 
         return "landing/index";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String DisplayRegisterForm(Model model) {
+
+        model.addAttribute(new user());
+        model.addAttribute("title", "Food Truck Finder");
+
+        return "register";
     }
 }
---
+++ src/main/java/launchcode/com/ftf/models/user.java
@@ -0,0 +1,70 @@
+package launchcode.com.ftf.models;
+
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class user {
+
+    @GeneratedValue
+    @Id
+    private int id;
+
+    @NotNull
+    @Size(min=6, max=12)
+    private String username;
+
+    @NotNull
+    @Size(min=6, max=20)
+    private String password;
+
+    @NotNull
+    @Email
+    private String email;
+
+    public user() {
+    }
+
+    public user(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
