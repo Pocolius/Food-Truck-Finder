0c285dd193cd829a4e66f07d383aae3959bcc7f1
Jared <jared.michael.wood@gmail.com>
2018-08-07 02:03:06 -0500

    Added a simple styles.css file. Landing page now has a nav bar and displays a form via model.

--- src/main/java/launchcode/com/ftf/controllers/LandingController.java
+++ src/main/java/launchcode/com/ftf/controllers/LandingController.java
@@ -1,15 +1,19 @@
 package launchcode.com.ftf.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class Landing {
+@RequestMapping(value = "landing")
+public class LandingController {
 
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Welcome to Food Truck Finder.";
+    public String index(Model model) {
+
+        model.addAttribute("title", "Landing");
+
+        return "landing/index";
     }
 }
